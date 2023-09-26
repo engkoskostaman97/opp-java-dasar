901c2281c83909b16d598925c147d17a5b7fa3a8
engkos kostaman <engkos.kostaman@bni.co.id>
2023-09-26 17:35:58 +0700

    field oop java

--- src/main/java/belajar/oop/java/belajar/java/Person.java
+++ src/main/java/belajar/oop/java/belajar/java/Person.java
@@ -1,4 +1,7 @@
 package belajar.oop.java.belajar.java;
 
 class Person {
+    String name;
+    String addres;
+    final String country = "INDONESIA";
 }
--- src/main/java/belajar/oop/java/belajar/java/PersonApp.java
+++ src/main/java/belajar/oop/java/belajar/java/PersonApp.java
@@ -2,14 +2,19 @@ package belajar.oop.java.belajar.java;
 
 public class PersonApp {
     public static void main(String[] args) {
+
         var person1 = new Person();
+        person1.name = "engkos";
+        person1.addres = "jakarta";
+
+        System.out.println(person1.name);
+        System.out.println(person1.addres);
+        System.out.println(person1.country);
+
         Person person2 = new Person();
         Person person3;
         person3 = new Person();
 
 
-        System.out.println(person1);
-        System.out.println(person2);
-        System.out.println(person3);
     }
 }
